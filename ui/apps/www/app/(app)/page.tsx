90875416a7812e6e6c46ce26d23e072ed414281a
manfromexistence <ajju40959@gmail.com>
2025-01-11 06:36:33 +0000

    feat: icon set page is done

--- ui/apps/www/app/(app)/page.tsx
+++ ui/apps/www/app/(app)/page.tsx
@@ -101,133 +101,139 @@ export default function IndexPage() {
               <ResizablePanelGroup direction="horizontal">
                 <ResizablePanel defaultSize={20}>
                   <ScrollArea className="h-full w-full p-4">
+                    {/* {iconSets.map(({ manifest, count, samples }) => (
+                      <Link
+                        key={manifest.id}
+                        className="hover:bg-primary-foreground flex h-[50px] w-full items-center justify-between rounded-md px-3"
+                        href={`/icons/${manifest.id}/`}
+                      >
+                        <span className="font-mono text-sm">{manifest.name}</span>
+                        <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">{count}</span>
+                      </Link>
+                    ))} */}
                     <Link href="/icons/ai" className="hover:bg-primary-foreground flex h-[50px] w-full items-center justify-between rounded-md px-3">
                       <span className="font-mono text-sm">Ant Design Icons</span>
-                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">831</span> 
+                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">831</span>
                     </Link>
                     <Link href="/icons/bs" className="hover:bg-primary-foreground flex h-[50px] w-full items-center justify-between rounded-md px-3">
                       <span className="font-mono text-sm">Bootstrap Icons</span>
-                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">2k</span> 
+                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">2716</span>
                     </Link>
                     <Link href="/icons/bi" className="hover:bg-primary-foreground flex h-[50px] w-full items-center justify-between rounded-md px-3">
                       <span className="font-mono text-sm">Boxicons</span>
-                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">1k</span> 
+                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">1634</span>
                     </Link>
                     <Link href="/icons/ci" className="hover:bg-primary-foreground flex h-[50px] w-full items-center justify-between rounded-md px-3">
                       <span className="font-mono text-sm">Circum Icons</span>
-                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">0</span> 
+                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">288</span>
                     </Link>
                     <Link href="/icons/cg" className="hover:bg-primary-foreground flex h-[50px] w-full items-center justify-between rounded-md px-3">
                       <span className="font-mono text-sm">css.gg</span>
-                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">0</span> 
+                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">704</span>
                     </Link>
                     <Link href="/icons/di" className="hover:bg-primary-foreground flex h-[50px] w-full items-center justify-between rounded-md px-3">
                       <span className="font-mono text-sm">Devicons</span>
-                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">0</span> 
+                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">192</span>
                     </Link>
                     <Link href="/icons/fa" className="hover:bg-primary-foreground flex h-[50px] w-full items-center justify-between rounded-md px-3">
                       <span className="font-mono text-sm">Font Awesome 4</span>
-                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">0</span> 
+                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">287</span>
                     </Link>
                     <Link href="/icons/fa6" className="hover:bg-primary-foreground flex h-[50px] w-full items-center justify-between rounded-md px-3">
                       <span className="font-mono text-sm">Font Awesome 6</span>
-                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">0</span> 
+                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">329</span>
                     </Link>
                     <Link href="/icons/fc" className="hover:bg-primary-foreground flex h-[50px] w-full items-center justify-between rounded-md px-3">
                       <span className="font-mono text-sm">Flat Color Icons</span>
-                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">0</span> 
+                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">1611</span>
                     </Link>
                     <Link href="/icons/fi" className="hover:bg-primary-foreground flex h-[50px] w-full items-center justify-between rounded-md px-3">
                       <span className="font-mono text-sm">Feather Icons</span>
-                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">0</span> 
+                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">2048</span>
                     </Link>
                     <Link href="/icons/gi" className="hover:bg-primary-foreground flex h-[50px] w-full items-center justify-between rounded-md px-3">
                       <span className="font-mono text-sm">Game Icons</span>
-                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">0</span> 
+                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">4040</span>
                     </Link>
                     <Link href="/icons/go" className="hover:bg-primary-foreground flex h-[50px] w-full items-center justify-between rounded-md px-3">
                       <span className="font-mono text-sm">Go Icons</span>
-                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">0</span> 
+                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">264</span>
                     </Link>
                     <Link href="/icons/gr" className="hover:bg-primary-foreground flex h-[50px] w-full items-center justify-between rounded-md px-3">
                       <span className="font-mono text-sm">Grommet Icons</span>
-                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">0</span> 
+                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">635</span>
                     </Link>
                     <Link href="/icons/hi" className="hover:bg-primary-foreground flex h-[50px] w-full items-center justify-between rounded-md px-3">
                       <span className="font-mono text-sm">Heroicons Outline</span>
-                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">0</span> 
+                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">460</span>
                     </Link>
                     <Link href="/icons/hi2" className="hover:bg-primary-foreground flex h-[50px] w-full items-center justify-between rounded-md px-3">
                       <span className="font-mono text-sm">Heroicons Solid</span>
-                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">0</span> 
+                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">972</span>
                     </Link>
                     <Link href="/icons/im" className="hover:bg-primary-foreground flex h-[50px] w-full items-center justify-between rounded-md px-3">
                       <span className="font-mono text-sm">Ionicons 4</span>
-                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">0</span> 
+                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">491</span>
                     </Link>
                     <Link href="/icons/io" className="hover:bg-primary-foreground flex h-[50px] w-full items-center justify-between rounded-md px-3">
                       <span className="font-mono text-sm">Ionicons 5</span>
-                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">0</span> 
+                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">1544</span>
                     </Link>
                     <Link href="/icons/io5" className="hover:bg-primary-foreground flex h-[50px] w-full items-center justify-between rounded-md px-3">
                       <span className="font-mono text-sm">Ionicons 5</span>
-                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">0</span> 
+                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">696</span>
                     </Link>
                     <Link href="/icons/lia" className="hover:bg-primary-foreground flex h-[50px] w-full items-center justify-between rounded-md px-3">
                       <span className="font-mono text-sm">Line Awesome</span>
-                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">0</span> 
+                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">1332</span>
                     </Link>
                     <Link href="/icons/lu" className="hover:bg-primary-foreground flex h-[50px] w-full items-center justify-between rounded-md px-3">
                       <span className="font-mono text-sm">Lucide</span>
-                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">0</span> 
+                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">1541</span>
                     </Link>
                     <Link href="/icons/md" className="hover:bg-primary-foreground flex h-[50px] w-full items-center justify-between rounded-md px-3">
                       <span className="font-mono text-sm">Material Design Icons</span>
-                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">0</span> 
+                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">4314</span>
                     </Link>
                     <Link href="/icons/md" className="hover:bg-primary-foreground flex h-[50px] w-full items-center justify-between rounded-md px-3">
-                      <span className="font-mono text-sm">Material Design Icons</span>
-                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">0</span> 
+                      <span className="font-mono text-sm">Phosphor Icons</span>
+                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">9072</span>
                     </Link>
                     <Link href="/icons/pi" className="hover:bg-primary-foreground flex h-[50px] w-full items-center justify-between rounded-md px-3">
-                      <span className="font-mono text-sm">Pixelart Icons</span>
-                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">0</span> 
+                      <span className="font-mono text-sm">Radix Icons</span>
+                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">318</span>
                     </Link>
                     <Link href="/icons/ri" className="hover:bg-primary-foreground flex h-[50px] w-full items-center justify-between rounded-md px-3">
                       <span className="font-mono text-sm">Remix Icon</span>
-                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">0</span> 
+                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">3020</span>
                     </Link>
                     <Link href="/icons/rx" className="hover:bg-primary-foreground flex h-[50px] w-full items-center justify-between rounded-md px-3">
-                      <span className="font-mono text-sm">React Icons/Rx</span> {/* This one is a bit unusual, might need clarification */}
-                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">0</span> 
+                      <span className="font-mono text-sm">Simple Icons</span>
+                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">3275</span>
                     </Link>
                     <Link href="/icons/si" className="hover:bg-primary-foreground flex h-[50px] w-full items-center justify-between rounded-md px-3">
-                      <span className="font-mono text-sm">Simple Icons</span>
-                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">0</span> 
+                      <span className="font-mono text-sm">Simple Line Icons</span>
+                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">189</span>
                     </Link>
                     <Link href="/icons/sl" className="hover:bg-primary-foreground flex h-[50px] w-full items-center justify-between rounded-md px-3">
-                      <span className="font-mono text-sm">Slime Icons</span>
-                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">0</span> 
+                      <span className="font-mono text-sm">Tabler Icons</span>
+                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">5754</span>
                     </Link>
                     <Link href="/icons/tb" className="hover:bg-primary-foreground flex h-[50px] w-full items-center justify-between rounded-md px-3">
-                      <span className="font-mono text-sm">Tabler Icons</span>
-                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">0</span> 
+                      <span className="font-mono text-sm">Themify Icons</span>
+                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">352</span>
                     </Link>
                     <Link href="/icons/tfi" className="hover:bg-primary-foreground flex h-[50px] w-full items-center justify-between rounded-md px-3">
-                      <span className="font-mono text-sm">Themify Icons</span>
-                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">0</span> 
-                    </Link>
-                    <Link href="/icons/ti" className="hover:bg-primary-foreground flex h-[50px] w-full items-center justify-between rounded-md px-3">
                       <span className="font-mono text-sm">Typicons</span>
-                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">0</span> 
+                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">336</span>
                     </Link>
                     <Link href="/icons/vsc" className="hover:bg-primary-foreground flex h-[50px] w-full items-center justify-between rounded-md px-3">
                       <span className="font-mono text-sm">VS Code Icons</span>
-                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">0</span> 
+                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">446</span>
                     </Link>
                     <Link href="/icons/wi" className="hover:bg-primary-foreground flex h-[50px] w-full items-center justify-between rounded-md px-3">
                       <span className="font-mono text-sm">Weather Icons</span>
-                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">0</span> 
+                      <span className="flex items-center justify-center rounded-full border p-2 font-mono text-xs">219</span>
                     </Link>
                   </ScrollArea>
                 </ResizablePanel>
@@ -267,26 +273,23 @@ export default function IndexPage() {
                     </div>
                   </div>
                   <ScrollArea className="h-full w-full">
-                    <div className="pb-24">
-                      <div className="grid grid-cols gap-4">
+                    <div className="mb-24">
+                      <div className="grid grid-cols-4 place-items-center gap-4 p-4">
                         {iconSets.map(({ manifest, count, samples }) => (
-                          <a
+                          <Link
                             key={manifest.id}
-                            className="m-5 rounded-md border"
+                            className="hover:bg-primary-foreground flex w-[285px] flex-col space-y-2 rounded-md border p-4"
                             href={`/icons/${manifest.id}/`}
                           >
-                            <div className="name">{manifest.name}</div>
-                            <div className="describe">
-                              <span className="counts">{count} icons</span>
-                            </div>
-                            <div className="samples">
-                              <div className="box">
+                            <div className="text-sm ">{manifest.name}</div>
+                            <div className="rounded-md border p-1">
+                              <div className="grid grid-cols-5 grid-rows-3 place-items-center gap-2 py-2">
                                 {samples.map((Component, i) => (
                                   <Component key={i} />
                                 ))}
                               </div>
                             </div>
-                          </a>
+                          </Link>
                         ))}
                       </div>
                     </div>
